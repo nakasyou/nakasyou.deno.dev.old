38ff82e057a56a381c271fd311a1aea2392e3a9e
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-19 14:06:22 +0900

    fix

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
-import * as hono from "npm:hono";
-import { jsx } from "npm:hono/jsx";
+import * as hono from "https://deno.land/x/hono/mod.ts";
+import { jsx } from "https://deno.land/x/hono/middleware.ts";
 import indexPage from "./components/index.tsx";
 
 const app = new hono.Hono()
