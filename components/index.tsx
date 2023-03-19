9ff0bbae8a352982a7dda78ca30f52be396303f5
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-19 13:32:47 +0900

    add

---
+++ components/index.tsx
@@ -0,0 +1,9 @@
+export default function(){
+    return (<html>
+        <head>
+        </head>
+        <body>
+            <h1>nakasyou</h1>
+        </body>
+    </html>)
+}
--- main.tsx
+++ main.tsx
@@ -1,9 +1,10 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 import * as hono from "npm:hono";
-import { jsx } from "npm:hono/jsx"
+import { jsx } from "npm:hono/jsx";
+import indexPage from "./components/index.tsx";
 
 const app = new hono.Hono()
 
-app.get('/', (c) => c.text('Hello Deno!'))
+app.get('/', (c) => c.html(indexPage))
 
 serve(app.fetch)
