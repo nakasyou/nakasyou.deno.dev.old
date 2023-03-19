dd97e0f2405578970a508a55555ee8563b6e2b5d
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-19 12:56:19 +0900

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,8 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
-import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { Hono } from 'npm:hono'
 
 const app = new Hono()
 
-app.get('/',ctx=>{
-  ctx.text("hello!")
-})
+app.get('/', (c) => c.text('Hello Deno!'))
 
 serve(app.fetch)
